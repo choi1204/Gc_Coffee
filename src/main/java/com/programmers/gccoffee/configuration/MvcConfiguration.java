0c9c2bc4f8ac16684367d34313e48b80019df770
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-05-08 23:21:38 +0900

    fix(Cors)

--- src/main/java/com/programmers/gccoffee/configuration/MvcConfiguration.java
+++ src/main/java/com/programmers/gccoffee/configuration/MvcConfiguration.java
@@ -9,6 +9,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .exposedHeaders("Location");
     }
 }
